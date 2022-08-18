cd656375c9951cd25507b181771c02e5e3c301c2
E Daley <100460009+3lyDaley@users.noreply.github.com>
2022-08-17 21:50:54 -0700

    Post, comment, inventory now seeding

--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -1,23 +1,23 @@
 const faker = require('faker');
 const userSeeds = require('./user-seeds.json');
-// const postSeeds = require('./post-seeds');
-// const commentSeeds = require('./comment-seeds');
-// const inventorySeeds = require('./inv-seeds');
+const postSeeds = require('./post-seeds');
+const commentSeeds = require('./comment-seeds');
+const inventorySeeds = require('./inv-seeds');
 const db = require('../config/connection')
 const { User, Comment, Inventory, Post } = require('../models')
 
 db.once('open', async () => {
   try {
     await User.deleteMany({});
-    // await Comment.deleteMany({});
-    // await Inventory.deleteMany({});
-    // await Post.deleteMany({});
+    await Comment.deleteMany({});
+    await Inventory.deleteMany({});
+    await Post.deleteMany({});
     
 
     await User.create(userSeeds);
-    // await Comment.create(commentSeeds)
-    // await Inventory.create(inventorySeeds)
-    // await Post.create(postSeeds)
+    await Comment.create(commentSeeds)
+    await Inventory.create(inventorySeeds)
+    await Post.create(postSeeds)
 
     // for (let i = 0; i < commentSeeds.length; i++) {
     //   const { _id, commentAuthor } = await Comment.create(commentSeeds[i]);
