fa40cea201bb2c1d00a36d44f149d17bb7255313
E Daley <100460009+3lyDaley@users.noreply.github.com>
2022-08-19 18:34:33 -0700

    deleted previous front end(moved file out of folder temporarily)

--- models/index.js
+++
@@ -1,6 +0,0 @@
-const User = require('./User');
-const Post = require('./Post');
-const Inventory = require('./Inventory');
-const Comment = require('./Comment')
-
-module.exports = { User, Post, Inventory, Comment }
