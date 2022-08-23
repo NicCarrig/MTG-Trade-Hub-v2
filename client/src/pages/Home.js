904d86b0c035987fd29f86951e8af3bea3cb360d
E Daley <100460009+3lyDaley@users.noreply.github.com>
2022-08-23 00:20:47 -0700

    CSS and some pages and stuff

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
+import { QUERY_POSTS } from '../utils/queries.js';
+import PostList from '../components/PostList'
+
 
 const Home = () => {
-    return (
-        <main>
-            <div className='flex-row justify-space-between'>
-                <div className='col-12 mb-3'>{/* Prints Posts */}</div>
-            </div>
-        </main>
-    );
+  // use useQuery hook to make query request
+  const { data, loading } = useQuery(QUERY_POSTS);
+
+  const post = data?.posts || [];
+  console.log(post);
+
+  return (
+    <main>
+ 
+      <div className="flex-row justify-space-between">
+        <div className="col-12 mb-3">
+          {loading ? (
+            <div>Loading...</div>
+            ) : (
+              < PostList posts={post} />
+              )}
+        </div>
+      </div>
+      
+    </main>
+  );
 };
 
 export default Home;
