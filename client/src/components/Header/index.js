5cc1fad898db4781c77325f6da8acbd26dc58c6c
sakau <sakau@usc.edu>
2022-08-24 19:01:57 -0700

    added nav link

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -10,38 +10,39 @@ const Header = () => {
     Auth.logout();
   };
 
-    return (
-        <header className="p-3 ">
-          <div className="header flex-row">
-            <nav className="navbar">
-              <div className='title'>
-                <Link to="/home">
-                  <h1>Magic The Gathering</h1>
-                  </Link>
-              </div>
-             <div className="navLinks">
-                  {Auth.loggedIn() ? (
-                    <>
-                  <Link to="/profile">Me</Link>
-                  <a href="/" onClick={logout}>
-                    Logout
-                  </a>
-                </>
-                  ) : (
-                <>
-                  <Link to="/login">Login</Link>
-                  <Link to="/signup">Signup</Link>
-                </>
-              )}
-             </div>
-            </nav>
+  return (
+    <header className="p-3 ">
+      <div className="header flex-row">
+        <nav className="navbar">
+          <div className='title'>
+            <Link to="/home">
+              <h1>Magic The Gathering</h1>
+            </Link>
           </div>
-        </header>
-          
+          <div className="navLinks">
+            {Auth.loggedIn() ? (
+              <>
+                <Link to="/profile">Me</Link>
+                <a href="/" onClick={logout}>
+                  Logout
+                </a>
+              </>
+            ) : (
+              <>
+                <Link to="/login">Login</Link>
+                <Link to="/signup">Signup</Link>
+                <Link to="/cards">Card Search</Link>
+              </>
+            )}
+          </div>
+        </nav>
+      </div>
+    </header>
 
-         
-            
-    );
+
+
+
+  );
 };
 
 export default Header;
